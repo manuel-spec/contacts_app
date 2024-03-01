c49a917bedaaff2214c80911bd79986600cede64
manuel-spec <amaniase084@gmail.com>
2024-03-01 08:43:34 +0300

    child routes rendering

--- src/main.jsx
+++ src/main.jsx
@@ -4,17 +4,21 @@ import './index.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Root from './routes/root'
 import ErrorPage from './ErrorPage'
+import Contact from './routes/contact'
 
 const router = createBrowserRouter([
   {
     index: '/',
     element: <Root />,
-    errorElement: <ErrorPage />
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "contacts/:contactId",
+        element: <Contact />,
+      },
+    ]
   },
-  {
-    path: "contacts/:contactId",
-    element: <Contact />,
-  },
+
 ])
 
 ReactDOM.createRoot(document.getElementById('root')).render(

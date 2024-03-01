e0d216217537fd98ebf8a6d265c3370f9b8b0285
manuel-spec <amaniase084@gmail.com>
2024-03-01 08:25:11 +0300

    get and set contact on local storage a fake net integration

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -49,3 +49,25 @@ export async function deleteContact(id) {
     }
     return false;
 }
+
+function set(contacts) {
+    return localforage.setItem("contacts", contacts);
+}
+
+// fake a cache so we don't slow down stuff we've already seen
+let fakeCache = {};
+
+async function fakeNetwork(key) {
+    if (!key) {
+        fakeCache = {};
+    }
+
+    if (fakeCache[key]) {
+        return;
+    }
+
+    fakeCache[key] = true;
+    return new Promise(res => {
+        setTimeout(res, Math.random() * 800);
+    });
+}

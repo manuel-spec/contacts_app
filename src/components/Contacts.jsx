7cad1e4742f59c2ac120bfc2cb2f1485a5b73097
manuel-spec <amaniase084@gmail.com>
2024-03-01 08:23:53 +0300

    update contact local storage

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -29,3 +29,12 @@ export async function getContact(id) {
     return contact ?? null;
 }
 
+export async function updateContact(id, updates) {
+    await fakeNetwork();
+    let contacts = await localforage.getItem("contacts");
+    let contact = contacts.find(contact => contact.id === id);
+    if (!contact) throw new Error("No contact found for", id);
+    Object.assign(contact, updates);
+    await set(contacts);
+    return contact;
+}
